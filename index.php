17ea80018d112dc2e3ca84d57eb92ec4d42fada5
unknown <labscrupnvjt@gmail.com>
2020-02-10 11:11:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+ <head>
+  <title>PHP-Test</title>
+ </head>
+ <body>
+  <?php
+    $varAwal = 1;
+    $varAkhir = 10;
+    $jumalha = 0;
+    
+    for ($i = $varAwal; $i <= $varAkhir; $i++){
+      $jumlah +=$i;
+      echo jumlah.", ";
+    }
+    echo "<br>";
+    echo $jumlah;
+  ?>
+ </body>
+
+</html>
